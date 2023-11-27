3ac01e12c9413474adc19b8f57762043f1524a54
bmaguire10 <123507408+bmaguire10@users.noreply.github.com>
2023-11-27 13:27:55 -0500

    Update node.cpp

--- node.cpp
+++ node.cpp
@@ -1,5 +1,23 @@
-//
-// Created by Benjamin Maguire on 11/27/2023.
-//
-//another test
-//test for git pull
+#include "node.h"
+
+// Default constructor
+Node::Node(){
+    this->data = 0;
+    this->next = nullptr;
+}
+
+Node::Node(int data){
+    this->data = data;
+    this->next = nullptr;
+}
+
+Node::Node(int data, Node* next){
+    this->data = data;
+    this->next = next;
+}
+
+Node::~Node(){
+    if(this->next != nullptr){
+        delete this->next;
+    }
+}
