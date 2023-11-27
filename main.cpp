8e107f8120dad7df68d28eabe7cd326f22c8dda9
Benjamin Maguire <bmaguire10@uri.edu>
2023-11-27 17:17:10 -0500

    update

--- main.cpp
+++ main.cpp
@@ -1,10 +1,22 @@
 #include <iostream>
+#include <fstream>
+#include <vector>
+#include <sstream>
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+int main(int argc, char* argv[]) {
+    std::ifstream inputFile(argv[1]);
+    std::vector<std::vector<int>> numbers;
+
+    std::string line;
+    while (std::getline(inputFile, line)) {
+        std::istringstream iss(line);
+        double num;
+        std::vector<int> lineNumbers;
+        while (iss >> num) {
+            lineNumbers.push_back(num);
+        }
+        numbers.push_back(lineNumbers);
+    }
+    inputFile.close(); // Close the file
+
 }
-
-//another test
-//test for bmaguire10
-//test again
