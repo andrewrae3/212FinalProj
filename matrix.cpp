9122bd60543d2faf5218b1ce7fe8159f70fa2bf6
AlejandroWu12 <alejandrowu@uri.edu>
2023-11-28 14:41:45 -0500

    matrix

--- matrix.cpp
+++ matrix.cpp
@@ -1,2 +1,15 @@
 #include "matrix.h"
 
+Node::Node(int row, int col, int data){
+    this->row = row;
+    this->col = col;
+    this->data = data;
+}
+
+SparseMatrix::SparseMatrix(){
+
+}
+
+SparseMatrix::SparseMatrix(int row, int col, int data){
+    
+}
