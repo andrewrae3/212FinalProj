ff13947faff09b35898af9fad750496a36074c8b
AlejandroWu12 <123507543+AlejandroWu12@users.noreply.github.com>
2023-11-29 15:27:24 -0500

    Update matrix.cpp

--- matrix.cpp
+++ matrix.cpp
@@ -1,11 +1,13 @@
 #include "matrix.h"
+#include <iostream>
+#include <vector>
 
 SparseMatrix::SparseMatrix(){
 
 }
 
 SparseMatrix::SparseMatrix(int row, int col, int data){
-   this->insert(row, col, data);
+    this->insert(row, col, data);
 }
 
 void SparseMatrix::insert(int row, int col, int data){
@@ -13,3 +15,60 @@ void SparseMatrix::insert(int row, int col, int data){
     this->matrix.push_back(node);
 }
 
+std::list<Node> SparseMatrix::add(SparseMatrix &matrix1, SparseMatrix &matrix2, int row, int col){
+    for (int i = 0; i <= row; i++) {
+        for (int j = 0; j <= col; j++) {
+            int value = matrix1.getElement(i, j) + matrix2.getElement(i, j);
+            Node node = {i,j,value};
+            if (value != 0) {
+                matrix.push_back(node);
+//                std::cout << "Added: Matrix1[" << i << "][" << j << "] + Matrix2[" << i << "][" << j << "] = " << value << std::endl;
+            }
+        }
+    }
+    return matrix;
+}
+
+std::list<Node> SparseMatrix::subtract(SparseMatrix &matrix1, SparseMatrix &matrix2, int row, int col){
+    for (int i = 0; i <= row; i++) {
+        for (int j = 0; j <= col; j++) {
+            int value = matrix1.getElement(i, j) - matrix2.getElement(i, j);
+            Node node = {i,j,value};
+            if (value != 0) {
+                matrix.push_back(node);
+//                std::cout << "Subtracted: Matrix1[" << i << "][" << j << "] + Matrix2[" << i << "][" << j << "] = " << value << std::endl;
+            }
+        }
+    }
+    return matrix;
+}
+
+std::list<Node> SparseMatrix::multiply(SparseMatrix &matrix1, SparseMatrix &matrix2, int row1, int col1, int row2, int col2){
+
+}
+
+std::list<Node> SparseMatrix::transpose(SparseMatrix &matrix1, int row1, int col1){
+    for (int j = 0; j <= col1; j++) {
+        for (int i = 0; i <= row1; i++) {
+            int value = matrix1.getElement(j, i);
+            if (value != 0) {
+                Node node = {i,j,value};
+                matrix.push_back(node);
+            }
+        }
+    }
+    return matrix;
+}
+
+int SparseMatrix::getElement(int row, int col) {
+    for (Node &node : matrix) {
+        if (node.row == row && node.col == col) {
+            return node.data;
+        }
+    }
+    return 0;
+}
+
+void SparseMatrix::print(){
+
+}
