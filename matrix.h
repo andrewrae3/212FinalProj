a24ebb949a0c422bb2258dc9f0dcb532cd2d29e4
andrewrae3 <123669931+andrewrae3@users.noreply.github.com>
2023-11-29 11:37:15 -0500

    Update matrix.h

--- matrix.h
+++ matrix.h
@@ -1,1 +1,30 @@
+#ifndef FINLA_MATRIX_H
+#define FINLA_MATRIX_H
 
+#include <list>
+struct Node{
+    int row;
+    int col;
+    int data;
+};
+
+class SparseMatrix{
+private:
+    std::list<Node> matrix;
+
+public:
+    SparseMatrix();
+    int getsize();
+  void MatrixToLL(std::vector<std::vector<int>>MatrixVec1);
+  void printList();
+    void add(std::list<Node> &matrix1, std::list<Node> &matrix2);
+    void subtract(std::list<Node> &matrix1, std::list<Node> &matrix2);
+    void multiply(std::list<Node> &matrix1, std::list<Node> &matrix2);
+    void divide(std::list<Node> &matrix1, std::list<Node> &matrix2);
+    void print(std::list<Node> &matrix);
+
+};
+
+
+
+#endif //FINLA_MATRIX_H
