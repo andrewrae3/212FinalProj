276cc815a749310679ee9e613d266c451fba1ef5
bmaguire10 <123507408+bmaguire10@users.noreply.github.com>
2023-12-01 09:55:30 -0500

    Update matrix.cpp

--- matrix.cpp
+++ matrix.cpp
@@ -1,312 +1,110 @@
+#include "matrix.h"
 #include <iostream>
 #include <vector>
-#include <sstream>
-#include <list>
-#include "matrix.h"
-#include <windows.h>
 
-void SetOutputText(const std::list<Node>& outputData);
-void SaveMatrixFromGUI(HWND matrixEdit, std::vector<std::vector<int>>& matrix);
+SparseMatrix::SparseMatrix(){
 
-const char *windowClassName = "Matrix Calculator";
-HWND editMatrix1, editMatrix2, output1;
-HWND btnMultiply, btnAdd, btnSubtract, btnTranspose, btnClear;
-SparseMatrix result;
-std::list<Node> temp;
-int Row1, Col1, Row2, Col2;
-std::ostringstream oss;
+}
 
-LRESULT CALLBACK WindowProcedure(HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam) {
-    switch (message) {
-        case WM_CREATE:
-// Create label for Matrix 1
-            CreateWindow("STATIC", "Matrix 1:",
-                         WS_CHILD | WS_VISIBLE,
-                         50, 30, 100, 20,
-                         hwnd, NULL, GetModuleHandle(NULL), NULL);
+SparseMatrix::SparseMatrix(int row, int col, int data){
+    this->insert(row, col, data);
+}
 
-            // Create first matrix input box
-            editMatrix1 = CreateWindowEx(WS_EX_CLIENTEDGE, "EDIT", "",
-                                         WS_CHILD | WS_VISIBLE | ES_MULTILINE | ES_AUTOVSCROLL | ES_AUTOHSCROLL,
-                                         50, 50, 200, 200,
-                                         hwnd, NULL, GetModuleHandle(NULL), NULL);
+void SparseMatrix::insert(int row, int col, int data){
+    Node node = {row, col, data};
+    this->matrix.push_back(node);
+}
 
-            // Create label for Matrix 2
-            CreateWindow("STATIC", "Matrix 2:",
-                         WS_CHILD | WS_VISIBLE,
-                         300, 30, 100, 20,
-                         hwnd, NULL, GetModuleHandle(NULL), NULL);
-
-            // Create second matrix input box
-            editMatrix2 = CreateWindowEx(WS_EX_CLIENTEDGE, "EDIT", "",
-                                         WS_CHILD | WS_VISIBLE | ES_MULTILINE | ES_AUTOVSCROLL | ES_AUTOHSCROLL,
-                                         300, 50, 200, 200,
-                                         hwnd, NULL, GetModuleHandle(NULL), NULL);
-            // Create 'Multiply' button
-            btnMultiply = CreateWindow("BUTTON", "Multiply",
-                                       WS_CHILD | WS_VISIBLE | BS_PUSHBUTTON,
-                                       50, 280, 100, 30,
-                                       hwnd, (HMENU)1, GetModuleHandle(NULL), NULL);
-
-            // Create 'Add' button
-            btnAdd = CreateWindow("BUTTON", "Add",
-                                  WS_CHILD | WS_VISIBLE | BS_PUSHBUTTON,
-                                  170, 280, 100, 30,
-                                  hwnd, (HMENU)2, GetModuleHandle(NULL), NULL);
-
-            // Create 'Subtract' button
-            btnSubtract = CreateWindow("BUTTON", "Subtract",
-                                       WS_CHILD | WS_VISIBLE | BS_PUSHBUTTON,
-                                       290, 280, 100, 30,
-                                       hwnd, (HMENU)3, GetModuleHandle(NULL), NULL);
-
-            // Create 'Transpose' button
-            btnTranspose = CreateWindow("BUTTON", "Transpose",
-                                        WS_CHILD | WS_VISIBLE | BS_PUSHBUTTON,
-                                        410, 280, 100, 30,
-                                        hwnd, (HMENU)4, GetModuleHandle(NULL), NULL);
-
-            btnClear = CreateWindow("BUTTON", "Clear",
-                                         WS_CHILD | WS_VISIBLE | BS_PUSHBUTTON,
-                                         530, 280, 100, 30,
-                                         hwnd, (HMENU)5, GetModuleHandle(NULL), NULL);
-
-            CreateWindow("STATIC", "Output:",
-                         WS_CHILD | WS_VISIBLE,
-                         550, 30, 100, 20,
-                         hwnd, NULL, GetModuleHandle(NULL), NULL);
-            output1 = CreateWindowEx(WS_EX_CLIENTEDGE, "EDIT", "",
-                                     WS_CHILD | WS_VISIBLE | ES_MULTILINE | ES_AUTOVSCROLL | ES_AUTOHSCROLL,
-                                     550, 50, 200, 200,
-                                     hwnd, NULL, GetModuleHandle(NULL), NULL);
-            break;
-        case WM_COMMAND:
-            switch (LOWORD(wParam)) {
-                case 1: // Multiply button clicked
-                    // Perform multiplication operation
-                    {
-                        std::vector<std::vector<int>> MatrixVec1;
-                        std::vector<std::vector<int>> MatrixVec2;
-                        SaveMatrixFromGUI(editMatrix1, MatrixVec1);
-                        SaveMatrixFromGUI(editMatrix2, MatrixVec2);
-
-                        // Set Row1, Col1, Row2, Col2 based on the sizes of MatrixVec1 and MatrixVec2
-                        Row1 = MatrixVec1.size();
-                        Col1 = (Row1 > 0) ? MatrixVec1[0].size() : 0;
-                        Row2 = MatrixVec2.size();
-                        Col2 = (Row2 > 0) ? MatrixVec2[0].size() : 0;
-
-                        // Perform matrix operations if sizes are valid
-                        if (Col1 == Row2) {
-                            SparseMatrix smatrix1;
-                            SparseMatrix smatrix2;
-
-                            // Convert MatrixVec1 to smatrix1
-                            for (size_t i = 0; i < Row1; ++i) {
-                                for (size_t j = 0; j < Col1; ++j) {
-                                    smatrix1.insert(i, j, MatrixVec1[i][j]);
-                                }
-                            }
-
-                            // Convert MatrixVec2 to smatrix2
-                            for (size_t i = 0; i < Row2; ++i) {
-                                for (size_t j = 0; j < Col2; ++j) {
-                                    smatrix2.insert(i, j, MatrixVec2[i][j]);
-                                }
-                            }
-
-                            // Perform multiplication operation
-                            temp = result.multiply(smatrix1, smatrix2, Row1, Col2);
-                            SetOutputText(temp);
-                        } else {
-                            // Handle invalid matrix sizes
-                            MessageBox(hwnd, "Invalid Matrix Sizes for Multiplication", "Error", MB_ICONERROR | MB_OK);
-                        }
-                    }
-                    break;
-                case 2: // Add button clicked
-                    // Perform addition operation
-                    {
-                        std::vector<std::vector<int>> MatrixVec1;
-                        std::vector<std::vector<int>> MatrixVec2;
-                        SaveMatrixFromGUI(editMatrix1, MatrixVec1);
-                        SaveMatrixFromGUI(editMatrix2, MatrixVec2);
-
-                        int Row1 = MatrixVec1.size();
-                        int Col1 = (Row1 > 0) ? MatrixVec1[0].size() : 0;
-                        int Row2 = MatrixVec2.size();
-                        int Col2 = (Row2 > 0) ? MatrixVec2[0].size() : 0;
-
-                        if (Row1 != Row2 || Col1 != Col2) {
-                            MessageBox(hwnd, "Invalid Input of Matrix Size", "Error", MB_ICONERROR | MB_OK);
-                        } else {
-                            SparseMatrix smatrix1;
-                            SparseMatrix smatrix2;
-
-                            for (size_t i = 0; i < Row1; ++i) {
-                                for (size_t j = 0; j < Col1; ++j) {
-                                    smatrix1.insert(i, j, MatrixVec1[i][j]);
-                                    smatrix2.insert(i, j, MatrixVec2[i][j]);
-                                }
-                            }
-
-                            temp = result.add(smatrix1, smatrix2, Row1, Col1);
-                            SetOutputText(temp);
-                        }
-                    }
-                    break;
-                case 3: // Subtract button clicked
-                    // Perform subtraction operation
-                    {
-                        std::vector<std::vector<int>> MatrixVec1;
-                        std::vector<std::vector<int>> MatrixVec2;
-                        SaveMatrixFromGUI(editMatrix1, MatrixVec1);
-                        SaveMatrixFromGUI(editMatrix2, MatrixVec2);
-
-                        int Row1 = MatrixVec1.size();
-                        int Col1 = (Row1 > 0) ? MatrixVec1[0].size() : 0;
-                        int Row2 = MatrixVec2.size();
-                        int Col2 = (Row2 > 0) ? MatrixVec2[0].size() : 0;
-
-                        if (Row1 != Row2 || Col1 != Col2) {
-                            MessageBox(hwnd, "Invalid Input of Matrix Size", "Error", MB_ICONERROR | MB_OK);
-                        } else {
-                            SparseMatrix smatrix1;
-                            SparseMatrix smatrix2;
-
-                            for (size_t i = 0; i < Row1; ++i) {
-                                for (size_t j = 0; j < Col1; ++j) {
-                                    smatrix1.insert(i, j, MatrixVec1[i][j]);
-                                    smatrix2.insert(i, j, MatrixVec2[i][j]);
-                                }
-                            }
-
-                            temp = result.subtract(smatrix1, smatrix2, Row1, Col1);
-                            SetOutputText(temp);
-                        }
-                    }
-                    break;
-                case 4: // Transpose button clicked
-                    // Perform transpose operation
-                    {
-                        std::vector<std::vector<int>> MatrixVec1;
-                        std::vector<std::vector<int>> MatrixVec2;
-                        SaveMatrixFromGUI(editMatrix1, MatrixVec1);
-                        SaveMatrixFromGUI(editMatrix2, MatrixVec2);
-
-                        int Row1 = MatrixVec1.size();
-                        int Col1 = (Row1 > 0) ? MatrixVec1[0].size() : 0;
-
-                            SparseMatrix smatrix1;
-
-                            // Convert MatrixVec1 to smatrix1
-                            for (size_t i = 0; i < Row1; ++i) {
-                                for (size_t j = 0; j < Col1; ++j) {
-                                    smatrix1.insert(i, j, MatrixVec1[i][j]);
-                                }
-                            }
-
-                            temp = result.transpose(smatrix1, Row1, Col1);
-                            SetOutputText(temp);
-                    }
-                    break;
-                case 5: // Clear button clicked
-                    // Clear the values in the output text window
-                    SetWindowText(output1, ""); // Clear the text in output1
-                    // Clear the values in the output list (temp)
-                    temp.clear(); // This line clears the 'temp' list
-                    // Clear the values in the stringstream (oss)
-                    oss.str(""); // Clear the content in the ostringstream
-                    oss.clear(); // Reset error flags in the ostringstream
-                    // Clear other lists or data structures if present
-                    // For example, clearing the 'result' SparseMatrix
-                    result.clear();
-                    break;
+std::list<Node> SparseMatrix::add(SparseMatrix &matrix1, SparseMatrix &matrix2, int row, int col){
+    for (int i = 0; i <= row; i++) {
+        for (int j = 0; j <= col; j++) {
+            int value = matrix1.getElement(i, j) + matrix2.getElement(i, j);
+            Node node = {i,j,value};
+            if (value != 0) {
+                matrix.push_back(node);
+//                std::cout << "Added: Matrix1[" << i << "][" << j << "] + Matrix2[" << i << "][" << j << "] = " << value << std::endl;
             }
-            break;
-        case WM_DESTROY:
-            PostQuitMessage(0);
-            break;
-        default:
-            return DefWindowProc(hwnd, message, wParam, lParam);
+        }
+    }
+    return matrix;
+}
+
+std::list<Node> SparseMatrix::subtract(SparseMatrix &matrix1, SparseMatrix &matrix2, int row, int col){
+    for (int i = 0; i <= row; i++) {
+        for (int j = 0; j <= col; j++) {
+            int value = matrix1.getElement(i, j) - matrix2.getElement(i, j);
+            Node node = {i,j,value};
+            if (value != 0) {
+                matrix.push_back(node);
+//                std::cout << "Subtracted: Matrix1[" << i << "][" << j << "] + Matrix2[" << i << "][" << j << "] = " << value << std::endl;
+            }
+        }
+    }
+    return matrix;
+}
+
+std::list<Node> SparseMatrix::multiply(SparseMatrix &matrix1, SparseMatrix &matrix2, int row1, int col2){
+    std::vector<std::vector<int>> tempmatrix(row1, std::vector<int>(col2, 0));
+    for (Node &node1 : matrix1.matrix) {
+        for (Node &node2 : matrix2.matrix) {
+            if (node1.col == node2.row) {
+                tempmatrix[node1.row][node2.col] += node1.data * node2.data;
+            }
+        }
+    }
+    for (int i = 0; i < row1; ++i) {
+        for (int j = 0; j < col2; ++j) {
+            if (tempmatrix[i][j] != 0) {
+                Node node = {i, j, tempmatrix[i][j]};
+                matrix.push_back(node);
+            }
+        }
+    }
+    return matrix;
+}
+
+//std::list<Node> SparseMatrix::power(SparseMatrix &matrix1, int row1, int col1, int op) {
+//
+//}
+//
+//std::list<Node> SparseMatrix::inverse(SparseMatrix &matrix1, int row1, int col1) {
+//
+//}
+//std::list<Node> SparseMatrix::determinant(SparseMatrix &matrix1, int row1, int col1) {
+//    if (row1 == 1) {
+//        int det = matrix1.getElement(0, 0);
+////        std::cout << "Determinant of 1x1 matrix: " << det << std::endl;
+//        return matrix;
+//    } else if (row1 == 2) {
+//        int a = matrix1.getElement(0, 0);
+//        int b = matrix1.getElement(0, 1);
+//        int c = matrix1.getElement(1, 0);
+//        int d = matrix1.getElement(1, 1);
+//
+//        int det = a * d - b * c;
+////        std::cout << "Determinant of 2x2 matrix: " << det << std::endl;
+//        return matrix;
+//    }
+//}
+
+std::list<Node> SparseMatrix::transpose(SparseMatrix &matrix1, int row1, int col1){
+    for (int j = 0; j <= col1; j++) {
+        for (int i = 0; i <= row1; i++) {
+            int value = matrix1.getElement(j, i);
+            if (value != 0) {
+                Node node = {i,j,value};
+                matrix.push_back(node);
+            }
+        }
+    }
+    return matrix;
+}
+
+int SparseMatrix::getElement(int row, int col) {
+    for (Node &node : matrix) {
+        if (node.row == row && node.col == col) {
+            return node.data;
+        }
     }
     return 0;
-}
-
-int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow) {
-    WNDCLASS wc = {0};
-
-    wc.lpfnWndProc = WindowProcedure;
-    wc.hInstance = hInstance;
-    wc.lpszClassName = windowClassName;
-    wc.hCursor = LoadCursor(NULL, IDC_ARROW);
-
-    RegisterClass(&wc);
-
-    HWND hwnd = CreateWindowEx(
-            0,
-            windowClassName,
-            "Matrix Operations",
-            WS_OVERLAPPEDWINDOW,
-            CW_USEDEFAULT, CW_USEDEFAULT,
-            800, 400,
-            NULL, NULL, hInstance, NULL);
-
-    if (hwnd == NULL) {
-        MessageBox(NULL, "Window creation failed!", "Error", MB_ICONERROR | MB_OK);
-        return 0;
-    }
-
-    ShowWindow(hwnd, nCmdShow);
-    UpdateWindow(hwnd);
-
-    MSG msg = {0};
-    while (GetMessage(&msg, NULL, 0, 0)) {
-        TranslateMessage(&msg);
-        DispatchMessage(&msg);
-    }
-
-    return msg.wParam;
-}
-
-void SetOutputText(const std::list<Node>& outputData) {
-    for (const auto& node : outputData) {
-        oss << "Row: " << node.row << ", Column: " << node.col << ", Value: " << node.data << "\r\n";
-    }
-    // Get the text from the output stream
-    std::string outputText = oss.str();
-
-    // Set the text in the output1 window
-    SetWindowText(output1, outputText.c_str());
-}
-
-// Function to save matrix values from GUI to a double vector
-void SaveMatrixFromGUI(HWND matrixEdit, std::vector<std::vector<int>>& matrix) {
-    int rows, cols;
-    std::vector<int> rowValues;
-
-    // Get the number of rows and columns in the matrix
-    rows = SendMessage(matrixEdit, EM_GETLINECOUNT, 0, 0);
-    cols = SendMessage(matrixEdit, EM_LINELENGTH, 0, 0);
-
-    // Iterate through each row of the matrix
-    for (int i = 0; i < rows; ++i) {
-        // Clear the row values vector for the new row
-        rowValues.clear();
-
-        // Get the text of the current row
-        char buffer[1000];
-        SendMessage(matrixEdit, EM_GETLINE, i, (LPARAM)buffer);
-
-        // Tokenize the row text to extract individual values
-        std::istringstream iss(buffer);
-        int value;
-        while (iss >> value) {
-            // Store each value in the rowValues vector
-            rowValues.push_back(value);
-        }
-
-        // Push the rowValues vector (representing a row) into the matrix
-        matrix.push_back(rowValues);
-    }
 }
