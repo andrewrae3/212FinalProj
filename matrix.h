03ff0c145a7ae097499a9f4dea63980336801a0f
bmaguire10 <123507408+bmaguire10@users.noreply.github.com>
2023-12-02 13:48:01 -0500

    Update matrix.h

--- matrix.h
+++ matrix.h
@@ -17,7 +17,6 @@ public:
     SparseMatrix(int row, int col, int data);
     void insert(int row, int col, int data);
     int getElement(int row, int col);
-    void MatrixToLL(std::vector<std::vector<int>>MatrixVec1);
     std::list<Node> add(SparseMatrix &matrix1, SparseMatrix &matrix2, int row, int col);
     std::list<Node> subtract(SparseMatrix &matrix1, SparseMatrix &matrix2, int row, int col);
     std::list<Node> multiply(SparseMatrix &matrix1, SparseMatrix &matrix2, int row1, int col2);
@@ -25,7 +24,9 @@ public:
 //    std::list<Node> power(SparseMatrix &matrix1, int row1, int col1, int op);
 //    std::list<Node> inverse(SparseMatrix &matrix1, int row1, int col1);
 //    std::list<Node> determinant(SparseMatrix &matrix1, int row1, int col1);
-    void print(std::list<Node> &matrix);
+    void clear() {
+        matrix.clear(); // Clears the content of the list 'matrix'
+    }
 
 };
 
